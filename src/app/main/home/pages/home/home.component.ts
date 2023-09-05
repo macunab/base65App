2aa55bcfed4837ae951d329540eaba30e693e2d7
Marshall <mn.acunab@gmail.com>
2023-09-05 19:53:56 -0300

    add imgToBase64 function

--- src/app/main/home/pages/home/home.component.ts
+++ src/app/main/home/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +8,45 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class HomeComponent {
 
+  base64: string | ArrayBuffer | null = '';
+
   constructor( private fb: FormBuilder) {}
 
-  formImage: FormGroup = this.fb.group({});
+  imageForm: FormGroup = this.fb.group({
+    file: ['', [Validators.required]],
+    fileSource: ['', [Validators.required]]
+  });
 
-  formImageSubmit() {}
+  imageFormSubmit() {
+
+    const submitFile = this.imageForm.get('fileSource')?.value;
+    this.imgToBase64(submitFile);
+  }
+
+  // Detecta los cambios en el input file y guarda el file en el sourceFile
+  onFileChange(event: Event)  {
+
+    const inputElement = event.currentTarget as HTMLInputElement;
+    if(inputElement.files?.length! > 0) {
+      const inputFile = inputElement.files![0];
+      this.imageForm.patchValue({
+        fileSource: inputFile
+      })
+    }
+  } 
+  
+  // Convierte un archivo en string base64
+  imgToBase64 =  async (file: File) => {
+
+    return await new Promise<string | ArrayBuffer | null>((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener('loadend', () => {
+        console.log(reader.result);
+        this.base64 = reader.result;
+        resolve(reader.result);
+      });
+       reader.readAsDataURL(file);
+    })
+  }
 
 }
